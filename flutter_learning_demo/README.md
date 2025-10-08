e6a7489a5826fbdcab0bee06fef6182aa89aedf8
tayseer anwar <tayseer.anwar@linkdev.com>
2025-10-08 11:51:25 +0300

    inti project

---
+++ flutter_learning_demo/README.md
@@ -0,0 +1,3 @@
+# flutter_learning_demo
+
+A new Flutter project.
---
+++ flutter_learning_demo/android/app/src/main/kotlin/com/example/flutter_learning_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_learning_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
