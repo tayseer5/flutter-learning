2438bbbb1593fab7c5a13a2522b35a0c86a79723
tayseer anwar <tayseer.anwar@linkdev.com>
2025-10-21 16:17:15 +0300

    feat(noteApp): add a constant file contain all constants needed in app

---
+++ flutter_demo/lib/helpers/constant.dart
@@ -0,0 +1,19 @@
+
+
+// constants for safer field usage
+class HiveFieldIds {
+  static const noteTitle = 0;
+  static const noteContent = 1;
+}
+
+class HiveTypeIds {
+  static const note = 0;
+}
+
+class HiveBoxesName {
+   static const noteBox = 'notesBox';
+}
+
+class SharedPreferencesKeys {
+  static const themeMode = 'themeMode';
+}
