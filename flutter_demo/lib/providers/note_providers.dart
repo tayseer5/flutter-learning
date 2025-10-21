446129ccbf0f3c40235f985bc45c3c84867ea39b
tayseer anwar <tayseer.anwar@linkdev.com>
2025-10-21 16:19:43 +0300

    change note provider to deal with local storage save data not static data

--- flutter_demo/lib/providers/note_providers.dart
+++ flutter_demo/lib/providers/note_providers.dart
@@ -1,48 +1,45 @@
+import 'package:flutter_demo/managers/hive_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../model/noteModel.dart';
+import '../model/note_model.dart';
 
 
 final notesProvider = NotifierProvider<NotesNotifier, List<NoteModel>>(() => NotesNotifier());
+
 class NotesNotifier extends Notifier<List<NoteModel>> {
 
   @override
   List<NoteModel> build() {
-    return [
-      NoteModel(noteTitle: 'Note 1', noteContent: 'This is note 1'),
-      NoteModel(noteTitle: 'Note 2', noteContent: 'This is note 2'),
-      NoteModel(noteTitle: 'Note 3', noteContent: 'This is note 3'),
-      NoteModel(noteTitle: 'Note 4', noteContent: 'This is note 4'),
-      NoteModel(noteTitle: 'Note 5', noteContent: 'This is note 5'),
-      NoteModel(noteTitle: 'Note 6', noteContent: 'This is note 6'),
-      NoteModel(noteTitle: 'Note 7', noteContent: 'This is note 7'),
-      NoteModel(noteTitle: 'Note 8', noteContent: 'This is note 8'),
-    ];
+    return HiveManager.notesBox.values.toList();
   }
 
-  void addNote(String title, String content) {
+  Future<void> addNote(String title, String content) async { // Future<void> add(Note note) async {
     final newNote = NoteModel(
       noteTitle: title,
       noteContent: content,
     );
-    state = [newNote, ...state];
+   final box = HiveManager.notesBox;
+   await box.add(newNote);
+    state = box.values.toList();
   }
 
-  void editNote(int index, String title, String content) {
+  Future<void> editNote(int index, String title, String content) async {
     final updatedNote = NoteModel(
       noteTitle: title,
       noteContent: content,
     );
-    state = [
-      ...state.sublist(0, index),
-      updatedNote,
-      ...state.sublist(index + 1),
-    ];
+    final box = HiveManager.notesBox;
+     await box.putAt(index, updatedNote);
+     state = box.values.toList();
   }
 
-  void deleteNote(int index) {
-    state = [
-      ...state.sublist(0, index),
-      ...state.sublist(index + 1),
-    ];
+  Future<void>  deleteNote(int index) async{
+   final box = HiveManager.notesBox;
+    await box.deleteAt(index);
+    state = box.values.toList();
+  }
+
+  Future<void> refresh() async {
+    final box = HiveManager.notesBox;
+    state = box.values.toList();
   }
 }
